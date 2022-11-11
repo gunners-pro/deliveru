ec98c50cdafe8583ed48329265161dd77ff1cf1a
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-11 07:43:26 -0300

    feat: add route to list all client deliveries

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { ensureAuthenticateDeliveryman } from './middlewares/ensureAuthenticateD
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
+import { FindAllDeliveriesController } from './modules/clients/useCases/deliveries/FindAllDeliveriesController';
 import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
 import { FindAllWithoutEndDateController } from './modules/deliveries/useCases/findAllWithoutEndDate/FindAllWithoutEndDateController';
 import { UpdateDeliverymanController } from './modules/deliveries/useCases/updateDeliveryman/UpdateDeliverymanController';
@@ -13,6 +14,7 @@ import { CreateDeliverymanController } from './modules/deliveryman/useCases/crea
 export const routes = Router();
 
 const createClientController = new CreateClientController();
+const findAllDeliveriesClientController = new FindAllDeliveriesController();
 const authenticateClientController = new AuthenticateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const updateDeliverymanController = new UpdateDeliverymanController();
@@ -23,6 +25,11 @@ const authenticateDeliverymanController =
 
 routes.post('/client', createClientController.handle);
 routes.post('/client/authenticate', authenticateClientController.handle);
+routes.get(
+  '/client/deliveries',
+  ensureAuthenticateClient,
+  findAllDeliveriesClientController.handle,
+);
 routes.post(
   '/deliveryman/authenticate',
   authenticateDeliverymanController.handle,
